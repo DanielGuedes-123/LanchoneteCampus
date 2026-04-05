3f7fce49da16cb492f3f30317e07d50b2c7189e8
Daniel <guedescastro059@gmail.com>
2026-04-05 18:05:04 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/main/java/br/edu/bsi/prog2/lanchonetecampus/LanchoneteCampus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.bsi.prog2.lanchonetecampus;
+
+/**
+ *
+ * @author guede
+ */
+public class LanchoneteCampus {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new FormularioPedido().setVisible(true);
+        });
+    }
+}
